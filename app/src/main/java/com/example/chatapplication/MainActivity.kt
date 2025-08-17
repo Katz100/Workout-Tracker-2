33f838be48c0756dc0be2653f3580dbdbe6eba20
Katz100 <cody.hopkins108@gmail.com>
2025-08-17 15:48:12 -0700

    Move Nav into its own composable

--- app/src/main/java/com/example/chatapplication/MainActivity.kt
+++ app/src/main/java/com/example/chatapplication/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.chatapplication.Components.CustomDrawer
+import com.example.chatapplication.Nav.NavApp
 import com.example.chatapplication.NavigationScreens.HomePage
 import com.example.chatapplication.NavigationScreens.SignIn
 import com.example.chatapplication.NavigationScreens.SignUp
@@ -66,138 +67,8 @@ class MainActivity : ComponentActivity() {
             ChatApplicationTheme {
                 val navController = rememberNavController()
                 val userAuthViewModel: UserAuthViewModel = hiltViewModel()
-                NavHost(
-                    navController = navController,
-                    startDestination = SplashScreen
-                ) {
 
-                    // Check to see if the user's session is active
-                    composable<SplashScreen> {
-                        val session = userAuthViewModel.uiState.collectAsState().value.session
-                        LaunchedEffect(session) {
-                            Log.e(TAG, "session value: ${session.toString()}")
-                            when(session) {
-                                is SessionStatus.NotAuthenticated -> {
-                                    Log.e(TAG, "Session not authenticated: ${session.toString()}")
-                                    navController.navigate(SignInScreen) {
-                                        popUpTo(SplashScreen) { inclusive = true }
-                                    }
-                                }
-                                is SessionStatus.Authenticated -> {
-                                    Log.e(TAG, "Session authenticated: ${session.toString()}")
-                                    navController.navigate(HomePageScreen) {
-                                        popUpTo(SplashScreen) { inclusive = true }
-                                    }
-                                }
-
-                                is SessionStatus.Initializing -> {
-                                    Log.e(TAG, "Session initializing")
-                                }
-                                is SessionStatus.RefreshFailure -> {
-                                    Log.e(TAG, "Session Refresh Failure: ${session.toString()}")
-                                    navController.navigate(SignInScreen) {
-                                        popUpTo(SplashScreen) { inclusive = true }
-                                    }
-                                }
-                            }
-                        }
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator()
-                        }
-                    }
-                    composable<SignInScreen> {
-                        val signInViewModel: SignInViewModel = hiltViewModel()
-                        val signInUiState = signInViewModel.uiState.collectAsState().value
-                        val signInResponse = signInViewModel.response.collectAsState().value
-
-                        var hasNavigated by remember { mutableStateOf(false) }
-
-                         // When _response is now Successful, user has successfully logged in so navigate to next screen.
-                        if (signInResponse is NetworkResult.Success && signInResponse.data == true && !hasNavigated) {
-                            LaunchedEffect(Unit) {
-                                hasNavigated = true
-                                navController.navigate(HomePageScreen) {
-                                    popUpTo(SignInScreen) { inclusive = true }
-                                }
-                            }
-
-                        }
-
-                        SignIn(
-                            email = signInUiState.email,
-                            password = signInUiState.password,
-                            onEmailChange = { signInViewModel.onEmailChange(it) },
-                            onPasswordChange = { signInViewModel.onPasswordChange(it) },
-                            onSignUpButtonPressed = {
-                                navController.navigate(SignUpScreen)
-                            },
-                            onSignInButtonPressed = { email, password ->
-                                //remove captured parameters
-                                signInViewModel.onSignIn()
-                            },
-                            onGoogleSignIn = { googleIdToken, rawNonce ->
-                                signInViewModel.onGoogleSignIn(googleIdToken, rawNonce)
-                            },
-                            isError = signInResponse
-                        )
-
-                    }
-
-                    composable<SignUpScreen> {
-                        val signUpViewModel: SignUpViewModel = hiltViewModel()
-                        val signUpUiState = signUpViewModel.uiState.collectAsState().value
-                        val signUpResponse = signUpViewModel.response.collectAsState().value
-
-                        var waitForConfirmation by remember { mutableStateOf(false) }
-
-                        if (signUpResponse is NetworkResult.Success) {
-                            waitForConfirmation = true
-                        }
-                        //TODO: navigate to separate screen so user can't go back to sign in page.
-                        if (waitForConfirmation) {
-                            Column(
-                                modifier = Modifier.fillMaxSize(),
-                                verticalArrangement = Arrangement.Center,
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ) {
-                                Text("Please confirm your email.")
-                                CircularProgressIndicator()
-                            }
-
-                        } else {
-                            SignUp(
-                                displayName = signUpUiState.displayName,
-                                email = signUpUiState.email,
-                                password = signUpUiState.password,
-                                confirmPassword = signUpUiState.confirmPassword,
-                                onEmailChange = { signUpViewModel.onEmailChange(it) },
-                                onPasswordChange = { signUpViewModel.onPasswordChange(it) },
-                                onConfirmPasswordChange = {
-                                    signUpViewModel.onConfirmPasswordChange(
-                                        it
-                                    )
-                                },
-                                onDisplayNameChange = { signUpViewModel.onDisplayNameChange(it) },
-                                onSignUpButtonPressed = { signUpViewModel.onSignUp() },
-                                isError = signUpResponse
-                            )
-                        }
-                    }
-
-                    composable<HomePageScreen> {
-                        HomePage(
-                            onLogOut = {
-                                userAuthViewModel.logOut()
-                                navController.navigate(SignInScreen) {
-                                    popUpTo(HomePageScreen) { inclusive = true }
-                                }
-                            }
-                        )
-                    }
-                }
+                NavApp(navController, userAuthViewModel)
             }
         }
     }
