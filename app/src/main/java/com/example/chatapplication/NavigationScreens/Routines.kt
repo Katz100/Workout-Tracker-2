47a83debcb4ba427db313d18909335d70c20d2a4
Katz100 <cody.hopkins108@gmail.com>
2025-08-19 16:32:21 -0700

    show no routines found text

--- app/src/main/java/com/example/chatapplication/NavigationScreens/Routines.kt
+++ app/src/main/java/com/example/chatapplication/NavigationScreens/Routines.kt
@@ -1,10 +1,14 @@
 package com.example.chatapplication.NavigationScreens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.chatapplication.Components.RoutineCard
@@ -16,21 +20,32 @@ fun Routines(
     routinesViewModel: RoutinesViewModel = hiltViewModel()
 ) {
     val routines = routinesViewModel.usersRoutines.collectAsState().value
+    val isEmpty = routinesViewModel.isEmpty.collectAsState().value
 
-    LazyColumn(
-        modifier = modifier.fillMaxSize()
-    ) {
-        items(routines) { routine ->
-            RoutineCard (
-                title = routine.name,
-                subtitle = routine.description ?: "",
-                onStartClick = {
-                    // TODO: navigate to routine detail/start workout
-                },
-                onMenuClick = {
-                    // TODO: open menu actions (edit/delete/etc.)
-                }
-            )
+    if (isEmpty) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        ) {
+            Text("No routines found")
+        }
+    } else {
+        LazyColumn(
+            modifier = modifier.fillMaxSize()
+        ) {
+            items(routines) { routine ->
+                RoutineCard(
+                    title = routine.name,
+                    subtitle = routine.description ?: "",
+                    onStartClick = {
+                        // TODO: navigate to routine detail/start workout
+                    },
+                    onMenuClick = {
+                        // TODO: open menu actions (edit/delete/etc.)
+                    }
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/chatapplication/viewmodel/RoutinesViewModel.kt
+++ app/src/main/java/com/example/chatapplication/viewmodel/RoutinesViewModel.kt
@@ -6,6 +6,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.chatapplication.data.dto.RoutineDto
 import com.example.chatapplication.data.repository.RoutineRepository
+import com.example.chatapplication.domain.model.NetworkResult
 import com.example.chatapplication.domain.model.Routine
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -19,9 +20,19 @@ class RoutinesViewModel @Inject constructor(
     private val _usersRoutines = MutableStateFlow<List<Routine>>(listOf())
     val usersRoutines: StateFlow<List<Routine>> = _usersRoutines
 
-    // TODO: add loading
+    private val _isEmpty = MutableStateFlow<Boolean>(true)
+    val isEmpty: StateFlow<Boolean> = _isEmpty
 
     init {
+        viewModelScope.launch {
+            _usersRoutines.collect {
+                if (it.isEmpty()) {
+                    _isEmpty.value = true
+                } else {
+                    _isEmpty.value = false
+                }
+            }
+        }
         viewModelScope.launch {
             routineRepository.routineFlow.collect { routines ->
                 Log.d("ROUTINEVIEWMODEL", "Collected routines: $routines")
