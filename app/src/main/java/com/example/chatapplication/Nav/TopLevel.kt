1891d6e87eb738021b8e595dc118520dc508de04
Katz100 <cody.hopkins108@gmail.com>
2025-08-21 09:51:35 -0700

    change bottom bar icons

--- app/src/main/java/com/example/chatapplication/Nav/TopLevel.kt
+++ app/src/main/java/com/example/chatapplication/Nav/TopLevel.kt
@@ -1,7 +1,10 @@
 package com.example.chatapplication.Nav
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Build
+import androidx.compose.material.icons.rounded.DateRange
 import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -12,22 +15,22 @@ enum class TopLevelDestination(
     val screen: Screen,
 ) {
     ROUTINE(
-        icon = Icons.Rounded.Home,
+        icon = Icons.Rounded.Menu,
         label = "Routine",
         screen = Screen.Routine,
     ),
     EXERCISE(
-        icon = Icons.Rounded.Person,
+        icon = Icons.Rounded.Build,
         label = "Exercises",
         screen = Screen.Exercise
     ),
     HISTORY(
-        icon = Icons.Rounded.Settings,
+        icon = Icons.Rounded.DateRange,
         label = "History",
         screen = Screen.History,
     ),
     PROFILE(
-        icon = Icons.Rounded.Settings,
+        icon = Icons.Rounded.Person,
         label = "Profile",
         screen = Screen.Profile,
     ),
