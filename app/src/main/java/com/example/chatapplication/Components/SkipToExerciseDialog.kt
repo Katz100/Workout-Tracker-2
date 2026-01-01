57ef2ea7d9fb11e17cf27db1178604623c24fcc2
Katz100 <cody.hopkins108@gmail.com>
2026-01-01 13:27:49 -0800

    add padding, horizontal line

--- app/src/main/java/com/example/chatapplication/Components/SkipToExerciseDialog.kt
+++ app/src/main/java/com/example/chatapplication/Components/SkipToExerciseDialog.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -66,9 +67,12 @@ fun SkipToExerciseDialog(
                 )
             }
 
+            HorizontalDivider()
+
             Column (
                 modifier = Modifier
                     .fillMaxSize()
+                    .padding(8.dp)
                     .verticalScroll(rememberScrollState())
             ) {
                 exercises.forEach { exercise ->
